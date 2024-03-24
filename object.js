71f0793827a338c549e6fbe769e20c23cf82eff3
janita tauqeer <janitatauqeer17@gmail.com>
2024-03-24 12:48:00 -0700

    Object with types

---
+++ object.js
@@ -0,0 +1,33 @@
+let person1 = {
+    name: "Suman",
+    class: 10,
+    rollNo: 17,
+    subjects: ["Math", "English", "Urdu", "Computer"],
+    address: {
+        city: "Karachi",
+        streetNo: 35,
+        streetName: "Akbar Road",
+    },
+};
+console.log(person1);
+let person2 = [
+    {
+        name: "Utraj",
+        class: 9,
+        rollNo: 9,
+        subjects: ["Accounting", "Banking", "Geography"],
+        address: {
+            city: "Karachi",
+            streetNo: 303,
+            streetName: "preedy st",
+        },
+    },
+];
+console.log(person2);
+let motorbike = "Ninja H2R";
+console.log(motorbike);
+let age = 22;
+console.log(age);
+let isWindy = true;
+console.log(isWindy);
+export {};
---
+++ object.ts
@@ -0,0 +1,56 @@
+// interface addressTypes {
+//   city: string;
+//   streetNo: number;
+//   streetName: string;
+// }
+// type personType = {
+//   name: string;
+//   class: number;
+//   rollNo: number;
+//   subjects: string[];
+//   address: addressTypes;
+// };
+
+// let person1: personType = {
+//   name: "Suman",
+//   class: 10,
+//   rollNo: 17,
+//   subjects: ["Math", "English", "Urdu", "Computer"],
+//   address: {
+//     city: "Karachi",
+//     streetNo: 35,
+//     streetName: "Akbar Road",
+//   },
+// };
+// console.log(person1);
+
+// let person2: personType[] = [
+//   {
+//     name: "Utraj",
+//     class: 9,
+//     rollNo: 9,
+//     subjects: ["Accounting", "Banking", "Geography"],
+//     address: {
+//       city: "Karachi",
+//       streetNo: 303,
+//       streetName: "preedy st",
+//     },
+//   },
+// ];
+
+// console.log(person2);
+
+//number,string,boolean => types
+
+//type alise
+type motorbikestring = string;
+let motorbike: motorbikestring = "Ninja H2R";
+console.log(motorbike);
+
+type agenumber = number;
+let age: agenumber = 22;
+console.log(age);
+
+type isWindyboolean = boolean;
+let isWindy: isWindyboolean = true;
+console.log(isWindy);
